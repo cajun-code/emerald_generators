f22ffa6e7b8d06c5474ebfe0bdcc3b80a40a4756
Allan Davis <cajun.code@gmail.com>
2011-06-06 01:23:49 -0400

    Create hello rails app

---
+++ code/hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '206f8b1f7f0cd67ebb1a5caa2f70f9c9ae2b6e44f613a01a29a55dfac217f39ddc9cdb10575b6086f1a9cf76b572667de103ec273fb883a7b745fdebf1786779'
